9af3f2ffa43cd56e61d8d29542a804fc61aa702a
Floating-Ocean <sea113290980@qq.com>
2024-08-15 02:59:38 +0800

    patch03: 修复时区问题

--- model.js
+++ model.js
@@ -1,29 +1,36 @@
 const { db } = global.Hydro.service; // 数据库连接
 const coll = db.collection('paste');
 
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
+function toUTC8Date(utc){
+    const utcDate = new Date(utc + " UTC");
+    const utc8Date = new Date(utcDate.toLocaleString('en-GB', { timeZone: 'Asia/Shanghai', hour12: false }));
+    return utc8Date;
+}
+
 async function add(userId, username, title, content, isprivate) {
     const pasteId = String.random(32); // Hydro提供了此方法，创建一个长度为32的随机字符串
     // 使用 mongodb 为数据库驱动，相关操作参照其文档
     var d = new Date(),	str = '';
-    function dateFormat(fmt, date) {
-        let ret;
-        const opt = {
-            "Y+": date.getFullYear().toString(),        // 年
-            "m+": (date.getMonth() + 1).toString(),     // 月
-            "d+": date.getDate().toString(),            // 日
-            "H+": date.getHours().toString(),           // 时
-            "M+": date.getMinutes().toString(),         // 分
-            "S+": date.getSeconds().toString()          // 秒
-            // 有其他格式化字符需求可以继续添加，必须转化成字符串
-        };
-        for (let k in opt) {
-            ret = new RegExp("(" + k + ")").exec(fmt);
-            if (ret) {
-                fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
-            };
-        };
-        return fmt;
-    }
     str = dateFormat("YYYY-mm-dd HH:MM:SS", d);
     const result = await coll.insertOne({
         _id: pasteId,
@@ -53,11 +60,22 @@ async function edit(pasteID, userId, title, content, isprivate) {
 }
 
 async function get(pasteId) {
-    return await coll.findOne({ _id: pasteId });
+    const result = await coll.findOne({ _id: pasteId });
+    if (result === undefined){
+        return result;
+    }
+    result.date = toUTC8Date(result.time);
+    result.time = dateFormat("YYYY-mm-dd HH:MM:SS", result.date);
+    return result;
 }
 
 async function getUserPaste(userId) {
-    return await coll.find({ "owner": Number.parseInt(userId) }).sort({ time: -1 }).toArray();
+    var result = await coll.find({ "owner": Number.parseInt(userId) }).sort({ time: -1 }).toArray();
+    result.forEach((item, index, array) => {
+        array[index].date = toUTC8Date(array[index].time);
+        array[index].time = dateFormat("YYYY-mm-dd HH:MM:SS", array[index].date);
+    });
+    return result;
 }
 
 
